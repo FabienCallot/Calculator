4c81d3b15c7dd5b5b4d485ea0a7b6b175b1bd5f8
Seishingan <fab.callot@gmail.com>
2021-09-10 10:43:28 +0200

    global modification on the style

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ buttons.forEach((button) => {
   });
 });
 
-// "+=" dans la fonction au dessus permet de concaténer les chiffres dans la calculatrice. ex : 5 remplacé par 4 devient 54 grace à cet opérateur//
-
 equal.addEventListener ('click' , () => {
   result.textContent = eval(result.textContent);
 })
